2f25bc8ec922e2f8a6f4106b1551049b09f551c5
nnjyami <y@179.jp>
2022-04-09 09:29:46 +0900

    [Add] #1 Store 初期ファイル追加

---
+++ store/index.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+  colors: [],
+  patterns: ["a1", "a2"],
+  selectedPatterns: [],
+  selectedColors: [],
+});
+
+export const getters = {};
+
+export const mutations = {};
+
+export const actions = {};
