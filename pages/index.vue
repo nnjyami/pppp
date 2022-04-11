ad490e116a75977f03ea84a72419648db52dc4fe
nnjyami <y@179.jp>
2022-04-12 01:26:43 +0900

    [Add] #1 whitePapper ページ（草案）の作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,39 @@
 <template>
-  <div :class="pageClass">
-    <Canvas/>
-    <pattern-unit
-      v-for="pattern in patterns"
-      :key="pattern"
-      :patternName="pattern"
-      color="000000"
-      />
+  <div class="whitepaper">
+    <p class="whitepaper_mediumText">
+      人もブランドやプロダクトも、<br />必ず背景を持っています。<br /><br />
+      Backgounds NFT は<br />
+      オリジナルの色とパターンで<br />
+      さまざまな環境において、<br />
+      その人、そのブランドを象徴するような<br />
+      「背景」を提供します。
+    </p>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import PatternUnit from '../components/PatternUnit.vue'
 export default {
-  components: { PatternUnit },
-  name: 'IndexPage',
+  layout: 'app',
   computed: {
-    ...mapState(["patterns"]),
-    pageClass: function(){
-      return [
-        "bg-slate-50",
-      ];
-    }
+  },
+  methods: {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.whitepaper {
+  font-feature-settings: "palt";
+  font-weight: normal;
+  letter-spacing: 0.04em;
+  padding: 2rem;
+  &_mediumText {
+    font-size: 1.6rem;
+    margin-bottom: 3.2rem;
+  }
+  &_text {
+    font-size: 1rem;
+  }
+}
+</style>
